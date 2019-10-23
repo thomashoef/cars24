e0a53698ac5522679d0e3dd89e0e283937610bd6
Thomas Hoefkens <thomas.hoefkens@live.de>
2019-10-23 10:46:19 +0200

    Initial project creation (VS Mac) and model added for CarAdvert

---
+++ cars24/Enumerations/FuelType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace cars24.Enumerations
+{
+    public enum FuelType
+    {
+        Gasoline,
+        Diesel
+    }
+}
---
+++ cars24/Models/CarAdvert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using cars24.Enumerations;
+
+namespace cars24.Models
+{
+    public class CarAdvert
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public FuelType Fuel { get; set; }
+        public int Price { get; set; }
+        public bool IsNew { get; set; }
+        public int Mileage { get; set; }
+        public DateTime FirstRegistration { get; set; }
+    }
+}
